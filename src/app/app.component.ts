d869af22aaed1c8beb3996b4351c3ab172192290
aladar <annamaria.demartino@gmail.com>
2023-06-08 20:50:58 +0200

    Progetto finale Ho inserito buma come style ma dato che non funziona bene ho tolto due div iniziali presenti invece nel template di Stephen Quindi ho aggiunto un div con stile inline
    il codice funziona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,49 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  length = 0;
+
+  onChangeLength(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const parsedValue = parseInt(target.value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!£$%&?^@#]()';
+    let validChars = '';
+    let generatedPassword = '';
+
+    if (this.includeLetters) validChars += letters;
+    if (this.includeNumbers) validChars += numbers;
+    if (this.includeSymbols) validChars += symbols;
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
 }
